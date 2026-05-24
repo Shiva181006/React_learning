981fb9571a19f0329752ad537ace9a5a4bbd0d36
Shiva181006 <shivakasudhan817@gmail.com>
2026-05-24 12:54:03 +0530

    created child component

--- src/component/product/product.jsx
+++ src/component/product/product.jsx
@@ -1,92 +1,55 @@
 import "./product.css";
-const Product = () => {
+import Product from "./product_card";
+const App = () => {
   return (
     <div>
       <div className="product_main_container">
         <div className="product_container">
-          <div className="card">
-            <img
-              src="https://cdn.pixabay.com/photo/2015/05/27/19/14/bird-787103_960_720.jpg"
-              alt="Photo 1"
-            />
-            <div className="title">Haapy Birds</div>
-            <h3 className="price"> 500</h3>
-            <p className="lorem">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, distinctio.
-            </p>
-            <button className="button">Click Me</button>
-          </div>
-          <div className="card">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwVP0UuuZDY-vrQ0Dij83mp9Myr15OR8AOTQ&s"
-              alt=""
-            />
-            <div className="title">Haapy Birds</div>
-            <h3 className="price"> 500</h3>
-            <p className="lorem">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, distinctio.
-            </p>
-            <button className="button">Click Me</button>
-          </div>
-          <div className="card">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwVP0UuuZDY-vrQ0Dij83mp9Myr15OR8AOTQ&s"
-              alt=""
-            />
+          <Product
+            image="https://cdn.pixabay.com/photo/2015/05/27/19/14/bird-787103_960_720.jpg"
+            title="Happy Bird"
+            price="500"
+            description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          />
 
-            <div className="title">Haapy Birds</div>
-            <h3 className="price"> 500</h3>
-            <p className="lorem">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, distinctio.
-            </p>
-            <button className="button">Click Me</button>
-          </div>
-          <div className="card">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwVP0UuuZDY-vrQ0Dij83mp9Myr15OR8AOTQ&s"
-              alt=""
-            />
-            <div className="title">Haapy Birds</div>
-            <h3 className="price"> 500</h3>
-            <p className="lorem">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, distinctio.
-            </p>
-            <button className="button">Click Me</button>
-          </div>
-          <div className="card">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwVP0UuuZDY-vrQ0Dij83mp9Myr15OR8AOTQ&s"
-              alt=""
-            />
-            <div className="title">Haapy Birds</div>
-            <h3 className="price"> 500</h3>
-            <p className="lorem">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, distinctio.
-            </p>
-            <button className="button">Click Me</button>
-          </div>
-          <div className="card">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwVP0UuuZDY-vrQ0Dij83mp9Myr15OR8AOTQ&s"
-              alt=""
-            />
-            <div className="title">Haapy Birds</div>
-            <h3 className="price"> 500</h3>
-            <p className="lorem">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, distinctio.
-            </p>
-            <button className="button">Click Me</button>
-          </div>
+          <Product
+            image="https://cdn.pixabay.com/photo/2015/05/27/19/14/bird-787103_960_720.jpg"
+            title="Happy Bird"
+            price="500"
+            description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          />
+
+          <Product
+            image="https://cdn.pixabay.com/photo/2015/05/27/19/14/bird-787103_960_720.jpg"
+            title="Happy Bird"
+            price="500"
+            description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          />
+
+          <Product
+            image="https://cdn.pixabay.com/photo/2015/05/27/19/14/bird-787103_960_720.jpg"
+            title="Happy Bird"
+            price="500"
+            description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          />
+
+          <Product
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwVP0UuuZDY-vrQ0Dij83mp9Myr15OR8AOTQ&s"
+            title="Happy Bird"
+            price="500"
+            description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          />
+
+          <Product
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwVP0UuuZDY-vrQ0Dij83mp9Myr15OR8AOTQ&s"
+            title="Happy Bird"
+            price="500"
+            description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          />
         </div>
       </div>
     </div>
   );
 };
 
-export default Product;
+export default App;
---
+++ src/component/product/product_card.jsx
@@ -0,0 +1,16 @@
+import "./product.css";
+const Product = ({ image, title, price, description }) => {
+  return (
+    <div>
+      <div className="card">
+        <img src={image} alt="Photo " />
+        <div className="title">{title}</div>
+        <h3 className="price">{price}</h3>
+        <p className="lorem">{description}</p>
+        <button className="button">Click Me</button>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
