12188103d0141684c8a17c9c2a44f4593cabb634
Priyanshu1710 <guptapriyanshu1710@gmail.com>
2026-05-24 11:23:02 +0530

    Implemented scss.

--- src/component/hero_section/hero.jsx
+++ src/component/hero_section/hero.jsx
@@ -1,4 +1,5 @@
-import "./hero.css"
+// import "./hero.css"
+import "./hero.scss";
 const Hero = () => {
   return (
     <div>
@@ -9,26 +10,36 @@ const Hero = () => {
               <h4>Customer Review</h4>
             </div>
             <div className="content">
-              <h2> <b>We <u>make</u> your <br />product <u>BEautiful</u> <br /> and <u>smart</u></b> </h2>
-              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, cum.</h3>
+              <h2>
+                {" "}
+                <b>
+                  We <u>make</u> your <br />
+                  product <u>BEautiful</u> <br /> and <u>smart</u>
+                </b>{" "}
+              </h2>
+              <h3>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Numquam, cum.
+              </h3>
             </div>
             <div className="button">
-              
-              <button className="button1">Let's talk</button>
-              <button className="button2"><u>View showreel</u></button>
-              
+              <button className="button1 btn_class">Let's talk</button>
+              <button className="button2 btn_class">
+                <u>View showreel</u>
+              </button>
             </div>
-            <div className="states">
-
-            </div>
+            <div className="states"></div>
           </div>
           <div className="img_container">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTa--M-LC5fqIIIA8j04uH8sIrYyc9r1eq2A&s" alt="Image" />
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTa--M-LC5fqIIIA8j04uH8sIrYyc9r1eq2A&s"
+              alt="Image"
+            />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
